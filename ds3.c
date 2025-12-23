47c81eeef744e2b63b50e9ff2f75e73132fcb589
Dheeraj Jadhav <jadhavdheeraj129@gmail.com>
2025-12-23 09:29:43 +0530

    Add stack operations with menu-driven interface
    Implemented a menu-driven program for stack operations including push, pop, palindrome check, and display. Handled overflow and underflow situations.

--- ds3.c
+++ ds3.c
@@ -1,3 +1,18 @@
+Design, Develop and Implement a menu driven Program in C for the following operations on STACKof Integers (Array Implementation of Stack with maximum size MAX):
+
+a) Push an Element on to Stack.
+b) Pop an Element from Stack.
+c) Demonstrate how Stack can be used to check Palindrome.
+d) Demonstrate Overflow and Underflow
+situations on Stack.
+e) Display the status of Stack.
+f) Exit
+
+Support the program with appropriate functions for each of the above operations.
+
+
+
+
 #include<stdio.h> 
 #include<stdlib.h> 
 #define MAX 5 
@@ -105,4 +120,5 @@ void palindrome()
  { 
  printf("\nIt is not a palindrome number"); 
  } 
+
 }
